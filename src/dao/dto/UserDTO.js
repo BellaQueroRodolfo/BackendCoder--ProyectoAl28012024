49a31a35cc0464194c5e97565d3b6962f062ee1c
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2024-01-28 21:56:55 -0300

    Update UserDTO.js

--- src/dao/dto/UserDTO.js
+++ src/dao/dto/UserDTO.js
@@ -2,8 +2,8 @@ class UserDTO {
   constructor(user) {
     this.id = user._id;
     this.username = user.username;
-    this.role = user.role;
     this.email = user.email;
+    this.role = user.role;
   }
 }
 
