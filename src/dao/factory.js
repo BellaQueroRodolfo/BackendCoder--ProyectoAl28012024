2f9ea3c18fefcdd305040bcf5a8a60ecbb25f470
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2024-01-28 21:55:48 -0300

    Update factory.js

--- src/dao/factory.js
+++ src/dao/factory.js
@@ -1,11 +1,18 @@
-const { PremiumUserDAO, UserDAO } = require('./dao');
-
-const getDAO = (type) => {
-  if (type === 'premium') {
+const { PremiumUserDAO, UserDAO, PremiumCartDAO, CartDAO } = require('./index');
+const getUserDAO = (daoType) => {
+  if (daoType === 'premium') {
     return new PremiumUserDAO();
   } else {
     return new UserDAO();
   }
 };
 
-module.exports = { getDAO };
+const getCartDAO = (daoType) => {
+  if (daoType === 'premium') {
+    return new PremiumCartDAO();
+  } else {
+    return new CartDAO();
+  }
+};
+
+module.exports = { getUserDAO, getCartDAO };
